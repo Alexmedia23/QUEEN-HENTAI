8f3406c8491dbaee1f78a65c606630eea46d77a8
Alexmedia23 <146990253+Alexmedia23@users.noreply.github.com>
2023-10-22 13:24:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Queen Hentai', {
+say('JBOT', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By Dinuwa Official²⁰²³._`, {
+say(`'${name}' By JOSH._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
